d4538fc849e42668d0b4e4b642c90794bd35611d
ConnorHeggie <connor_heggie@yahoo.com>
2016-11-20 21:38:24 -0600

    added new function head for annotations

--- getImages.py
+++ getImages.py
@@ -2,6 +2,17 @@ import numpy as np
 import scipy as si
 import os
 
+def getFGmap(filename):
+    
+    script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+    abs_file_path = script_dir + '\annotations\trimaps' + '\'
+    finalpath = abs_file_path + filename
+    image = si.misc.imread(finalpath)
+    
+    
+    
+
+
 def getImages(filename): 
 
     script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
