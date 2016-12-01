0c2a9144da187778db76d546901290a5a2965696
BootstrapB <bjb7@rice.edu>
2016-11-30 21:54:27 -0600

    made it so data scales

--- testModels.py
+++ testModels.py
@@ -1,12 +1,14 @@
 from sklearn import svm
 from featureExtraction import datamatPaper
 import numpy as np
+from sklearn.preprocessing import scale
 
 # takes in trained svm object (sklearn object) returns fgmap for the testing images
 def testPaperSVM(clf, img):
     dims = img.shape
     
     xMat = datamatPaper(img)
+    xMat = scale(xMat)
     labels = clf.predict(xMat)
     
     return np.reshape(labels, (dims[0], dims[1]))
